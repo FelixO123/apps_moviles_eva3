0131a5c6a9439e56b1ed66785d5abc6cc24a5395
FelixO123 <fe.omenaca@duocuc.cl>
2025-12-04 22:08:12 -0300

    borrando_todo

--- src/main/java/com/example/miapp/repository/OrdenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.miapp.model.Orden;
-
-
-public interface OrdenRepository extends JpaRepository<Orden, Long> {
-}
--- src/main/java/com/example/miapp/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.miapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.miapp.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
